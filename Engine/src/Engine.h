59c1f2e7d0a355da293fe8c2fb9c343068de2c1f
Dhruv <dp3903@gmail.com>
2025-12-30 18:41:46 +0530

    Application setup and entrypoint setup.

---
+++ Engine/src/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by external applications only
+
+#include "Engine/Application.h"
+#include "Engine/Core.h"
+
+// -------Entry point--------
+#include "Engine/Entrypoint.h"
+// --------------------------
